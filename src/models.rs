95c8a59ba5b30777eb6033b818cf2a5fe97dcbb3
ThatGuyJamal <jamallyons8@gmail.com>
2023-05-03 18:38:04 -0400

    updated

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,5 @@
-#[allow(unused_imports)]
-
-// todo - fix datetime parsing errors
+// Database models for the application. This is a port from the web/prisma schema file in the 
+// Client side codebase.
 
 use chrono::{DateTime, Utc};
 use mongodb::bson::oid::ObjectId;
@@ -13,7 +12,7 @@ pub struct User {
     pub username: Option<String>,
     pub bio: Option<String>,
     pub email: String,
-    // pub email_verified: Option<DateTime<Utc>>,
+    pub email_verified: Option<DateTime<Utc>>,
     pub image: String,
     pub roles: Vec<UserRole>,
     pub telemetry: bool,
@@ -55,15 +54,15 @@ pub struct Session {
     pub id: ObjectId,
     pub session_token: String,
     pub user_id: ObjectId,
-    // pub expires: DateTime<Utc>,
+    pub expires: DateTime<Utc>,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Tokens {
     pub id: ObjectId,
     pub token: String,
-    // pub created_at: DateTime<Utc>,
-    // pub updated_at: Option<DateTime<Utc>>,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: Option<DateTime<Utc>>,
     pub tomestoned: bool,
     pub author_id: ObjectId,
     pub owner: User,
@@ -75,13 +74,12 @@ pub struct SystemReport {
     pub title: String,
     pub description: String,
     pub status: ReportStatus,
-    // pub created_at: DateTime<Utc>,
+    pub created_at: DateTime<Utc>,
     pub assigned_to_id: Option<ObjectId>,
     pub assigned_to: Option<User>,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
 pub enum ReportStatus {
     InProgress,
     RESOLVED,
