eacbf8bcbec917a1cabfc809e4eb44b759f1660c
ThatGuyJamal <jamallyons8@gmail.com>
2023-05-07 21:02:41 -0400

    updated

--- src/methods/post.rs
+++ src/methods/post.rs
@@ -6,11 +6,11 @@ use crate::{
 };
 use actix_web::{post, web, HttpResponse, Responder};
 use mongodb::bson::{doc, oid::ObjectId};
-use neura_labs_engine::{
-    pipelines::translation::generate_translation,
-    utils::{concatenate_strings, convert_strings_to_strs},
-    Language,
-};
+// use neura_labs_engine::{
+//     pipelines::translation::generate_translation,
+//     utils::{concatenate_strings, convert_strings_to_strs},
+//     Language,
+// };
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
@@ -18,22 +18,22 @@ pub struct GetUserBody {
     pub id: String,
 }
 
-#[derive(Deserialize, Clone)]
-pub struct TranslateBody {
-    /// The language to translate from
-    source_language: Language,
-    /// The language to translate to
-    target_language: Language,
-    /// The input to translate
-    input_context: Vec<String>,
-    concat: Option<bool>,
-    id: String,
-}
+// #[derive(Deserialize, Clone)]
+// pub struct TranslateBody {
+//     /// The language to translate from
+//     source_language: Language,
+//     /// The language to translate to
+//     target_language: Language,
+//     /// The input to translate
+//     input_context: Vec<String>,
+//     concat: Option<bool>,
+//     id: String,
+// }
 
-#[derive(Serialize, Clone)]
-struct TranslateResponse {
-    data: Vec<String>,
-}
+// #[derive(Serialize, Clone)]
+// struct TranslateResponse {
+//     data: Vec<String>,
+// }
 
 /// Translates a given input from a source language to a target language
 ///
@@ -53,71 +53,51 @@ struct TranslateResponse {
 ///   }
 /// }
 /// ```
-#[post("/api/v1/translate")]
-pub async fn translate(
-    data: web::Data<AppState>,
-    body: web::Json<RequestBody<TranslateBody>>,
-) -> impl Responder {
-    let body_data = body.data.clone();
+// #[post("/api/v1/translate")]
+// pub async fn translate(
+//     data: web::Data<AppState>,
+//     body: web::Json<RequestBody<TranslateBody>>,
+// ) -> impl Responder {
+//     let body_data = body.data.clone();
 
-    let id = data.db.convert_to_object_id(body_data.id).unwrap();
-    let can_run = data.db.process_credit_usage(id).await.unwrap();
+//     let id = data.db.convert_to_object_id(body_data.id).unwrap();
+//     let can_run = data.db.process_credit_usage(id).await.unwrap();
 
-    if !can_run {
-        return HttpResponse::BadRequest().body("Insufficient credit amount!");
-    }
+//     if !can_run {
+//         return HttpResponse::BadRequest().body("Insufficient credit amount!");
+//     }
 
-    let result = actix_web::web::block(move || {
-        // todo - make sure this works
-        let v_langs = vec![body_data.source_language, body_data.target_language];
-        for lang in v_langs {
-            let validate = validate_language_input(lang);
-            if !validate {
-                return Err::<TranslateResponse, anyhow::Error>(anyhow::Error::msg(
-                    "Invalid language input",
-                ));
-            }
-        }
+//     let result = actix_web::web::block(move || {
+//         let mut res = Vec::new();
 
-        let mut res = Vec::new();
+//         let data = generate_translation(
+//             body_data.source_language,
+//             body_data.target_language,
+//             convert_strings_to_strs(&body_data.input_context),
+//         )
+//         .unwrap();
 
-        let data = generate_translation(
-            body_data.source_language,
-            body_data.target_language,
-            convert_strings_to_strs(&body_data.input_context),
-        )
-        .unwrap();
+//         if body_data.concat.unwrap_or(false) {
+//             let c = concatenate_strings(&convert_strings_to_strs(&data));
+//             res.push(c)
+//         } else {
+//             res.extend(data);
+//         };
 
-        if body_data.concat.unwrap_or(false) {
-            let c = concatenate_strings(&convert_strings_to_strs(&data));
-            res.push(c)
-        } else {
-            res.extend(data);
-        };
+//         Ok::<TranslateResponse, anyhow::Error>(TranslateResponse { data: res })
+//     })
+//     .await
+//     .unwrap();
 
-        Ok::<TranslateResponse, anyhow::Error>(TranslateResponse { data: res })
-    })
-    .await
-    .unwrap();
-
-    match result {
-        Ok(res) => {
-            return HttpResponse::Ok().json(res);
-        }
-        Err(e) => {
-            return HttpResponse::InternalServerError().body(format!("{}", e));
-        }
-    };
-}
-
-fn validate_language_input(lang: Language) -> bool {
-    match lang {
-        Language::English => true,
-        Language::Spanish => true,
-        Language::French => true,
-        _ => false,
-    }
-}
+//     match result {
+//         Ok(res) => {
+//             return HttpResponse::Ok().json(res);
+//         }
+//         Err(e) => {
+//             return HttpResponse::InternalServerError().body(format!("{}", e));
+//         }
+//     };
+// }
 
 // todo - block users from accessing this endpoint
 #[post("/api/v1/token")]
