acdcb8c18bfdabe1b01b4eb936fb44b6cc54d336
ThatGuyJamal <jamallyons8@gmail.com>
2023-05-07 22:23:21 -0400

    testing shuttle

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,17 @@
-mod db;
-mod error;
-mod methods;
-mod middleware;
-mod utils;
-mod models;
+use actix_web::{get, web::ServiceConfig};
+use shuttle_actix_web::ShuttleActixWeb;
 
-extern crate anyhow;
-
-use actix_web::{middleware::Logger, web, App, HttpServer};
-use env_logger::Env;
-
-use methods::{
-    get::{health_check, index, get_global_statistics}, post::{create_api_token, create_user_payment},
-};
-
-#[derive(Clone, Debug)]
-pub struct AppState {
-    app_name: String,
-    db: db::MongoDB,
+#[get("/")]
+async fn hello_world() -> &'static str {
+    "Hello World!"
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let db = db::MongoDB::new(&utils::env().unwrap().mongodb_uri)
-        .await
-        .expect("Failed to initialize MongoDB");
+#[shuttle_runtime::main]
+async fn actix_web(
+) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    let config = move |cfg: &mut ServiceConfig| {
+        cfg.service(hello_world);
+    };
 
-    env_logger::init_from_env(Env::default().default_filter_or("info"));
-
-    HttpServer::new(move || {
-        let app_state = AppState {
-            app_name: String::from("Neura Labs API"),
-            db: db.clone(),
-        };
-
-        App::new()
-            .app_data(web::Data::new(app_state))
-            .wrap(Logger::default())
-            .wrap(Logger::new("%a %{User-Agent}i"))
-            .wrap(middleware::auth::RequestHandler)
-            // get
-            .service(index)
-            .service(health_check)
-            .service(get_global_statistics)
-            // post
-            // .service(translate)
-            .service(create_api_token)
-            .service(create_user_payment)
-    })
-    .bind((utils::env().unwrap().address, utils::env().unwrap().port))?
-    .run()
-    .await
+    Ok(config.into())
 }
